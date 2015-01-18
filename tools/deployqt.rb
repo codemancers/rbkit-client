1fe849dc1a4792df229f72c6b935af8551e3a261
Hemant Kumar <gethemant@gmail.com>
2015-01-18 08:54:31 -0500

    rename os_type to arch

--- tools/deployqt.rb
+++ tools/deployqt.rb
@@ -25,7 +25,7 @@ class DeployQt
 
   def remove_existing_files
     FileUtils.rm_rf(dst)
-    FileUtils.rm_rf("rbkit-#{os_type}.tar.gz")
+    FileUtils.rm_rf("rbkit-#{arch}.tar.gz")
   end
 
   def create_archive
@@ -38,7 +38,7 @@ class DeployQt
 
   def create_tar_file
     FileUtils.cd(@pwd) do
-      system("tar -zcvf rbkit-#{os_type}.tar.gz rbkit")
+      system("tar -zcvf rbkit-#{arch}.tar.gz rbkit")
     end
   end
 
@@ -86,10 +86,10 @@ export QT_QPA_FONTDIR=\`pwd\`/fonts
   end
 
   def compiler_version
-    os_type == 'i686' ? 'gcc' : 'gcc_64'
+    arch == 'i686' ? 'gcc' : 'gcc_64'
   end
 
-  def os_type
+  def arch
     `uname -m`.strip
   end
 end
