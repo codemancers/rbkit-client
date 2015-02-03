bd6c265820093ed76d775c2b62c83c617bbfc278
Yuva <yuva@codemancers.com>
2015-02-03 23:03:56 +0530

    [#issue-128] use select union, which has bumped up time taken to 37sec

--- tests/testbmheapsnapshot.cpp
+++ tests/testbmheapsnapshot.cpp
@@ -17,7 +17,7 @@ static QByteArray msgpackDataFromFile(const QString filename)
 void TestBmHeapSnapshot::initTestCase()
 {
     // read object dump, and parse it
-    auto objectDump = msgpackDataFromFile(":/tests/msgpack/hugedump");
+    auto objectDump = msgpackDataFromFile("/Users/yuva/00013");
     RBKit::EventParser eventParser(objectDump);
 
     auto collection = dynamic_cast<EvtCollection*>(eventParser.parseEvent());
@@ -63,27 +63,45 @@ void TestBmHeapSnapshot::testBenchmarkPersistingToDb()
         qDebug() << sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &error);
         qDebug() << error;
 
+        quint64 objectCount = 0, referenceCount = 0;
+        QString objectSql, referenceSql;
+
         for (auto& object : store->objectStore) {
-            sqlite3_bind_int64(objStmt, 1, object->objectId);
-            sqlite3_bind_text(objStmt,  2, object->className.toStdString().c_str(),
-                              object->className.size(), SQLITE_TRANSIENT);
-            sqlite3_bind_int64(objStmt, 3, object->size);
-            sqlite3_bind_int64(objStmt, 4, object->references.size());
-            sqlite3_bind_text(objStmt,  5, object->getFileLine().toStdString().c_str(),
-                              object->getFileLine().size(), SQLITE_TRANSIENT);
-
-            sqlite3_step(objStmt);
-            sqlite3_clear_bindings(objStmt);
-            sqlite3_reset(objStmt);
-
-            for (auto& ref : object->references) {
-                sqlite3_bind_int64(refStmt, 1, object->objectId);
-                sqlite3_bind_int64(refStmt, 2, ref);
-
-                sqlite3_step(refStmt);
-                sqlite3_clear_bindings(refStmt);
-                sqlite3_reset(refStmt);
+            if (objectCount == 500) {
+                sqlite3_exec(db, objectSql.toStdString().c_str(),
+                             NULL, NULL, &error);
+                objectSql.clear();
+                objectCount = 0;
             }
+
+            if (!objectCount) {
+                objectSql
+                    .append("INSERT INTO rbkit_objects SELECT ")
+                    .append(QString::number(object->objectId)).append(" AS id,'")
+                    .append(object->className).append("' AS class_name,")
+                    .append(QString::number(object->size)).append(" AS size,")
+                    .append(QString::number(object->references.size())).append(" AS reference_count,'")
+                    .append(object->getFileLine()).append("' AS file ");
+            } else {
+                objectSql
+                    .append("UNION SELECT ")
+                    .append(QString::number(object->objectId)).append(",'")
+                    .append(object->className).append("',")
+                    .append(QString::number(object->size)).append(",")
+                    .append(QString::number(object->references.size())).append(",'")
+                    .append(object->getFileLine()).append("' ");
+            }
+
+            ++objectCount;
+
+            // for (auto& ref : object->references) {
+            //     sqlite3_bind_int64(refStmt, 1, object->objectId);
+            //     sqlite3_bind_int64(refStmt, 2, ref);
+
+            //     sqlite3_step(refStmt);
+            //     sqlite3_clear_bindings(refStmt);
+            //     sqlite3_reset(refStmt);
+            // }
         }
 
         qDebug() << sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &error);
